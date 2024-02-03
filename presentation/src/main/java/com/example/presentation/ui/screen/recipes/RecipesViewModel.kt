def1c9f9b33c20eb9dc246c81623f602555dc012
paulhorda <paulhorda02480@gmail.com>
2024-02-04 00:21:43 +0200

    ARP-4-Create ViewModel for RecipesFragment

---
+++ presentation/src/main/java/com/example/presentation/ui/screen/recipes/RecipesViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.presentation.ui.screen.recipes
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class RecipesViewModel : ViewModel() {
+
+}
