c540aa74c417adea26acd1786568344ea2ba12df
paulhorda <paulhorda02480@gmail.com>
2024-02-04 00:20:56 +0200

    ARP-4-Create ViewModel for Favorites

---
+++ presentation/src/main/java/com/example/presentation/ui/screen/favorites/FavoritesViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.presentation.ui.screen.favorites
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class FavoritesViewModel : ViewModel() {
+}
