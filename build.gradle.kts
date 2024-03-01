4a6f11701c01e470ff707ab26a7c555dafbe1ec0
paulhorda <paulhorda02480@gmail.com>
2024-03-01 13:21:40 +0200

    ARP-10-Update project gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,6 @@ plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("com.android.library") version "8.2.0" apply false
-    id("com.google.dagger.hilt.android") version "2.44" apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
+    id("androidx.navigation.safeargs") version "2.7.7" apply false
 }
