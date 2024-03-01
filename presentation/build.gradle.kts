ee27cfbf4dd05991a19c749c73bafc1e572a8e2f
paulhorda <paulhorda02480@gmail.com>
2024-03-01 13:32:00 +0200

    ARP-11-Update gradle presentation module

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("com.google.dagger.hilt.android")
+    id("androidx.navigation.safeargs.kotlin")
     kotlin("kapt")
 }
 
@@ -39,21 +40,45 @@ android {
 dependencies {
     implementation(project(":domain"))
 
-    val hiltVersion = "2.49"
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+
+    val hiltVersion = "2.50"
 
     implementation("com.google.dagger:hilt-android:$hiltVersion")
     kapt("com.google.dagger:hilt-android-compiler:$hiltVersion")
 
-    val navigationVersion = "2.7.6"
+    val navigationVersion = "2.7.7"
 
     implementation("androidx.navigation:navigation-fragment-ktx:$navigationVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navigationVersion")
 
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.2.0-alpha01")
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
+
+    implementation("com.google.code.gson:gson:2.10.1")
+
 }
 
 kapt {
     correctErrorTypes = true
+}
+
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        val navigationVersion = "2.7.7"
+
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navigationVersion")
+    }
 }
